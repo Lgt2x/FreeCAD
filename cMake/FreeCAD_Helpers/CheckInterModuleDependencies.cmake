9f09d7fff058f66933da6bf902ad8793bab2b8d7
Louis Gombert <louisgombert@gmail.com>
2025-12-14 23:41:29 +0100

    CheckInterModuleDependencies: fix conditional
    'if(${dependent})' always evaluated to true, because 'dependent' is the name of the module, and not the value set when enabled. To fix the test, interpret the module name variable as a variable name

--- cMake/FreeCAD_Helpers/CheckInterModuleDependencies.cmake
+++ cMake/FreeCAD_Helpers/CheckInterModuleDependencies.cmake
@@ -5,7 +5,7 @@ macro(CheckInterModuleDependencies)
     # Takes a dependent module followed by a variable-length list of prerequisite
     # modules.  Warns if any of the prerequisite modules are disabled.
     function(REQUIRES_MODS dependent)
-        if(${dependent})
+        if("${${dependent}}")
             foreach(prerequisite IN LISTS ARGN)
                 if(NOT ${prerequisite})
                     message(SEND_ERROR "${dependent} requires ${prerequisite} to be ON, but it"
@@ -14,7 +14,7 @@ macro(CheckInterModuleDependencies)
                     break()
                 endif(NOT ${prerequisite})
             endforeach()
-        endif(${dependent})
+        endif()
     endfunction(REQUIRES_MODS)
 
     REQUIRES_MODS(BUILD_ASSEMBLY           BUILD_PART BUILD_PART_DESIGN BUILD_SPREADSHEET)
