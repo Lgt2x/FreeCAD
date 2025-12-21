68341cb86bb058d6011edcdcdcd87ee9ff265a0c
Louis Gombert <louisgombert@gmail.com>
2025-12-21 19:02:44 +0100

    CI: update native Linux build to Ubuntu 25.05 and Qt6

--- package/ubuntu/install-apt-packages.sh
+++ package/ubuntu/install-apt-packages.sh
@@ -2,15 +2,10 @@
 set -euo pipefail
 
 # Update package lists quietly
-sudo apt-get update -qq
-
-if apt-cache show libvtk9-dev >/dev/null 2>&1; then
-  vtk_dev="libvtk9-dev"
-else
-  vtk_dev="libvtk7-dev"
-fi
+apt-get update -qq
 
 packages=(
+  build-essential
   ccache
   cmake
   doxygen
@@ -35,13 +30,12 @@ packages=(
   libopencv-dev
   libproj-dev
   libpcl-dev
-  libpyside2-dev
-  libqt5opengl5-dev
-  libqt5svg5-dev
-  libqt5x11extras5-dev
+  libpyside6-dev
+  libqt6opengl6-dev
+  libqt6svg6-dev
   libshiboken2-dev
   libspnav-dev
-  ${vtk_dev}
+  libvtk9-dev
   libx11-dev
   libxerces-c-dev
   libyaml-cpp-dev
@@ -50,8 +44,8 @@ packages=(
   netgen-headers
   ninja-build
   occt-draw
-  pyqt5-dev-tools
-  pyside2-tools
+  python3-pip
+  pyside6-tools
   python3-dev
   python3-defusedxml
   python3-git
@@ -62,17 +56,19 @@ packages=(
   python3-pivy
   python3-ply
   python3-pybind11
-  python3-pyside2.qtcore
-  python3-pyside2.qtgui
-  python3-pyside2.qtnetwork
-  python3-pyside2.qtsvg
-  python3-pyside2.qtwidgets
-  qtbase5-dev
-  qttools5-dev
+  python3-pyside6.qtcore
+  python3-pyside6.qtgui
+  python3-pyside6.qtnetwork
+  python3-pyside6.qtsvg
+  python3-pyside6.qtwidgets
+  qt6-base-dev
+  qt6-l10n-tools
+  qt6-tools-dev
+  qt6-tools-dev-tools
   shiboken2
   swig
   xvfb
 )
 
 # Install all packages
-sudo apt-get install -y --no-install-recommends "${packages[@]}"
+apt-get install -y --no-install-recommends "${packages[@]}"
